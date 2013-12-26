37da8a9c253f2a798f3358026dc74982ce0cce09
Jared Grippe <jared@deadlyicon.com>
2013-12-26 00:42:04 -0800

    WIP

--- spec/lib/serveit/static_spec.rb
+++ spec/lib/serveit/static_spec.rb
@@ -40,4 +40,43 @@ describe Serveit::HamlTemplates do
     end
   end
 
+  context 'GET /' do
+    it "renders a 404" do
+      response = get '/'
+      expect( response.status  ).to eq 404
+      expect( response.body    ).to eq ""
+      expect( response.headers ).to eq("Content-Type"=>"text/plain")
+    end
+  end
+
+  context 'GET /index' do
+    it "renders a 404" do
+      response = get '/index'
+      expect( response.status  ).to eq 404
+      expect( response.body    ).to eq ""
+      expect( response.headers ).to eq("Content-Type"=>"text/plain")
+    end
+  end
+
+  context 'GET /index.html' do
+    it "renders a 404" do
+      response = get '/index.html'
+      expect( response.status  ).to eq 404
+      expect( response.body    ).to eq ""
+      expect( response.headers ).to eq("Content-Type"=>"text/plain")
+    end
+  end
+
+
+  context 'GET /index.html.haml' do
+    it "renders /index.html.haml as a 200" do
+      response = get '/index.html.haml'
+      expect( response.status  ).to eq 200
+      expect( response.body    ).to eq "%h1 Welcome to my app\n"
+      expect( response.headers['Content-Type']   ).to eq 'text/plain'
+      expect( response.headers['Content-Length'] ).to eq '22'
+      expect( response.headers['Last-Modified']  ).to_not be_nil
+    end
+  end
+
 end
